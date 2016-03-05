dfed10bc93cba8207b86ac45ea0c2a86ef6da7f8
Andi <andicbuchanan@gmail.com>
2016-03-05 13:49:56 +1300

    random story working

--- routes/index.js
+++ routes/index.js
@@ -11,22 +11,45 @@ var connection = mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('hi!')
-  connection.query('SELECT * FROM stories', function(err, rows){
-    console.log(res)
-    res.render('index', {title: 'glowworm', stories : rows });
-    console.log(rows)  
-  });
+  res.render('index');
 })
 
 /* GET stories page. */
 router.get('/stories/', function(req, res, next) {
   console.log('stories')
   connection.query('SELECT * FROM stories', function(err, rows){
-    console.log(res)
     res.render('stories', {title: 'glowworm', stories : rows });
-    console.log(rows)  
   });
 })
+
+// GET individual story page
+
+router.get('/stories/:id', function(req, res, next){
+  var id = req.params.id
+  if (id === 'random'){
+    connection.query('SELECT idstories FROM stories ORDER BY rand() LIMIT 1', function(err, rows){
+      var randomStoryId = rows[0].idstories
+      res.redirect('/stories/'+randomStoryId)
+    })
+  } else {
+    connection.query('SELECT * FROM stories WHERE idstories=' + id, function(err,rows){
+      console.log(id)
+      console.log(rows)
+      res.render('showstory', {title:'glowworm', story : rows[0]})
+    })    
+  }
+})
+
+// GET individual story edit page, edit and delete stories
+
+router.route('/stories/:id/edit')
+  .get(function(req, res, next){
+  var id = req.params.id
+  connection.query('SELECT * FROM stories WHERE idstories=' + id, function(err,rows){
+    console.log(rows)
+    res.render('editstory', {title:'glowworm', story : rows[0]})
+    })    
+  })
+  // .put(req, res, next)
 
 module.exports = router;
